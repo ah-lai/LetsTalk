91bd0f6d0b3b4b07ea2d6456d06176611f6309c4
ah-lai <lai.alvin189@gmail.com>
2019-07-18 13:56:12 -0400

    Update serializers.py
    update

--- chat/serializers.py
+++ chat/serializers.py
@@ -4,21 +4,26 @@ from .models import message, message_reciever, message_sender
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = message
-        fields = ('message_id','content')
+        fields = ('content')
 
 
 
 class SenderSerializer(serializers.ModelSerializer):
-    message_id = serializers.StringRelatedField(many=False)
-    user_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    message = MessageSerializer(many=True, read_only=True)
+    
     class Meta:
         model = message_sender
-        fields = ('user_id', 'message_id')
+        fields = ('message')
 
 
 class RecieverSerializer(serializers.ModelSerializer):
-    message_id = serializers.StringRelatedField(many=False)
-    user_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    message = MessageSerializer(many=True, read_only=True) 
+    # I think this will make it have content in this object (switch to primary key)
+    
     class Meta:
         model = message_reciever
-        fields = ('user_id','message_id')
+        fields = ('message')
+        
+        
+        
+   
